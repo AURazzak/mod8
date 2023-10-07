3c174cdb2e528b4fe4fa742c893667113a37367d
AURazzak <ahmedurrajjak@gmail.com>
2023-10-07 23:32:46 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,123 +8,226 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen(),
-
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      home: FirstScreen(),
     );
   }
 }
 
-class ProfileScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  Map<String, int> itemCounts = {"Denim": 1, "T-Shirt": 1, "Jacket": 1};
+  double unitPrice = 20.00;
+
+  void _incrementItem(String item) {
+    setState(() {
+      itemCounts[item] = itemCounts[item]! + 1;
+    });
+  }
+
+  void _decrementItem(String item) {
+    setState(() {
+      itemCounts[item] = itemCounts[item]! > 0 ? itemCounts[item]! - 1 : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-    final isLandscape = orientation == Orientation.landscape;
+    double totalAmount = itemCounts.values.fold(0, (previous, count) => previous + (count * unitPrice));
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        elevation: 0,
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: isLandscape
-            ? _buildLandscapeLayout()
-            : _buildPortraitLayout(),
-      ),
-    );
-  }
-
-  Widget _buildPortraitLayout() {
-    return Column(
-      children: <Widget>[
-        SizedBox(height: 8.0),
-        Center(
-          child: CircleAvatar(
-            radius: 160,
-            backgroundImage: NetworkImage(
-                'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F009%2F273%2F280%2Fnon_2x%2Fconcept-of-loneliness-and-disappointment-in-love-sad-man-sitting-element-of-the-picture-is-decorated-by-nasa-free-photo.jpg&tbnid=wCRKX8OXxT2N0M&vet=12ahUKEwj8kv_x8NKBAxXp5TgGHalnBbEQMygAegQIARBR..i&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Ffree-photos%2Fpicture&docid=wska7sM6RxRdCM&w=1470&h=980&q=picture&ved=2ahUKEwj8kv_x8NKBAxXp5TgGHalnBbEQMygAegQIARBR'),
-          ),
-        ),
-        SizedBox(height: 16.0),
-        Center(
-          child: Text(
-            'Naruto Uzumaki',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "My Bag",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
             ),
-          ),
-        ),
-        SizedBox(height: 16.0),
-        Text(
-          'This is the profile app task given to me by the Ostad team, and now I am completing it with flutter.',
-          style: TextStyle(
-            fontSize: 18,
-          ),
-        ),
-
-        SizedBox(height: 16.0),
-        _buildPhotoGrid(3),
-      ],
-    );
-  }
-
-  Widget _buildLandscapeLayout() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          flex: 2,
-          child: Center(
-            child: CircleAvatar(
-              radius: 160,
-              backgroundImage: NetworkImage(
-                  'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F474x%2F0f%2F4a%2Fba%2F0f4aba8348df3a41b51ae07371088190--i-care-moonlight.jpg&tbnid=yihrwjm8dd7siM&vet=12ahUKEwiGrKz38NKBAxVISWwGHUpUDxsQxiAoAXoECAAQGw..i&imgrefurl=https%3A%2F%2Fin.pinterest.com%2Farunhosur58%2Fcaller-image%2F&docid=ExtCQqqR6MESCM&w=474&h=711&itg=1&q=picture&ved=2ahUKEwiGrKz38NKBAxVISWwGHUpUDxsQxiAoAXoECAAQGw'),
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 8.0),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Naruto Uzumaki',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+            SizedBox(height: 30),
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildClothItem(
+                    "Denim",
+                    "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/6840942/2018/9/20/ed807de3-6988-4600-8c6e-a0fb61ce50691537446736708-Levis-Men-Green-Solid-Denim-Jacket-5381537446735225-1.jpg",
+                    "Green",
                   ),
-                ),
+                  _buildClothItem(
+                    "T-Shirt",
+                    "https://images.othoba.com/images/thumbs/0550099_mens-100-export-quality-premium-royal-blue-color-short-sleeve-t-shirt.jpeg",
+                    "Blue",
+                  ),
+                  _buildClothItem(
+                    "Jacket",
+                    "https://www.swanndri.co.nz/media/catalog/product/image/121608507/men-s-redwoods-softshell-jacket-with-fleece-lining.jpg",
+                    "Black",
+                  ),
+                ],
               ),
-              SizedBox(height: 16.0),
-              Text(
-                'This is the profile app task given to me by the Ostad team, and now I am completing it with flutter.',
-                style: TextStyle(
-                  fontSize: 18,
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Opacity(opacity: 0.7, child: Text("Total Amount")),
+                Text(
+                  "\$${totalAmount.toStringAsFixed(2)}",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
+              ],
+            ),
+            SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Congratulations on your checkout!")),
+                );
+              },
+              child: Text("CHECK OUT"),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.red,
+                textStyle: TextStyle(fontWeight: FontWeight.bold),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(130),
+                ),
+                minimumSize: Size(double.infinity, 50),
               ),
-              SizedBox(height: 16.0),
-              _buildPhotoGrid(3),
-            ],
+            ),
+            SizedBox(height: 12),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildClothItem(String title, String imageUrl, String color) {
+    Color clothColor;
+    if (color == "Green") {
+      clothColor = Colors.green;
+    } else if (color == "Blue") {
+      clothColor = Colors.blue;
+    } else if (color == "Black") {
+      clothColor = Colors.black;
+    } else {
+      clothColor = Colors.red;
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 120,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+                        Row(
+                          children: [
+                            Opacity(opacity: 0.8, child: Text("Color:")),
+                            SizedBox(width: 6),
+                            Text(color),
+                            SizedBox(width: 17),
+                            Opacity(opacity: 0.8, child: Text("Size:")),
+                            SizedBox(width: 3),
+                            Text("M"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            _buildCircleIcon(Icons.remove, () => _decrementItem(title)),
+                            SizedBox(width: 12),
+                            Text("${itemCounts[title]}", style: TextStyle(fontWeight: FontWeight.bold)),
+                            SizedBox(width: 12),
+                            _buildCircleIcon(Icons.add, () => _incrementItem(title)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Opacity(
+                          opacity: 0.7,
+                          child: Icon(
+                            Icons.more_vert,
+                            size: 32,
+                          ),
+                        ),
+                        Text(
+                          "\$${(unitPrice * itemCounts[title]!).toStringAsFixed(2)}",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
+        SizedBox(height: 15),
       ],
     );
   }
 
-  Widget _buildPhotoGrid(int crossAxisCount) {
-    return GridView.count(
-      shrinkWrap: true,
-      crossAxisCount: crossAxisCount,
-      children: List.generate(6, (index){
-        final imageUrl =
-            'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2Fb3%2F02%2Fd1%2Fb302d1ced0244be8eec910b31b5e125f.jpg&tbnid=cT0zbL-BPk3CnM&vet=12ahUKEwiGrKz38NKBAxVISWwGHUpUDxsQxiAoA3oECAAQFw..i&imgrefurl=https%3A%2F%2Fwww.pinterest.co.uk%2Fpin%2Fthats-one-giant-leap-for-mankind-cleverly-posed-photos-show-man-jumping-over-the-moon-holding-it-and-even-slam-dunking-it--193021534002334108%2F&docid=q-FumkYgGdabjM&w=634&h=859&itg=1&q=picture&ved=2ahUKEwiGrKz38NKBAxVISWwGHUpUDxsQxiAoA3oECAAQFw';
-        return Padding(
-          padding: EdgeInsets.all(4.0),
-          child: Image.network(imageUrl, fit: BoxFit.cover),
-        );
-      }),
+  Widget _buildCircleIcon(IconData iconData, VoidCallback onPressed) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              blurRadius: 12,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Center(child: Icon(iconData, color: Colors.black.withOpacity(0.75), size: 22)),
+      ),
     );
   }
 }
